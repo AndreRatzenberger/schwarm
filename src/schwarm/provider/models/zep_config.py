7e039248c96da5c9fa161fce309cfccc197c3733
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2024-11-19 18:20:18 +0100

    context

--- src/schwarm/provider/models/zep_config.py
+++ src/schwarm/provider/models/zep_config.py
@@ -6,5 +6,6 @@ from schwarm.provider.models import BaseEventHandleProviderConfig
 class ZepConfig(BaseEventHandleProviderConfig):
     """Configuration for the Zep provider."""
 
-    zep_api_key: str = ""
+    zep_api_key: str = "zepzep"
     zep_url: str = "http://localhost:8000"
+    min_fact_rating: float = 0.3
